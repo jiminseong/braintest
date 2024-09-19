fd41b2909c415e80bb9f9603c6dd10f220a5957d
jiminseong <iamjms4237@gachon.ac.kr>
2024-09-19 14:16:08 +0900

    feat/프린트 기능 임시 구현

--- src/pages/test-result/TestResultPage.tsx
+++ src/pages/test-result/TestResultPage.tsx
@@ -1,14 +1,48 @@
 import styled from 'styled-components';
+import { useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
 
 const TestResultPage = () => {
-    return <PrintContainer>프린트할 공간이에요</PrintContainer>;
+    const componentRef = useRef<HTMLDivElement | null>(null);
+
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+        documentTitle: '결과페이지',
+    });
+    return (
+        <PageWrapper>
+            <PrintButton onClick={handlePrint}>Print</PrintButton>
+            <PrintContainer ref={componentRef}>프린트할 공간이에요</PrintContainer>;
+        </PageWrapper>
+    );
 };
 
 export default TestResultPage;
 
+const PageWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1em;
+    background: blue;
+`;
 const PrintContainer = styled.div`
-    width: 50%;
-    height: 50%;
+    width: 1000px;
+    height: 300px;
     background: #ffffff;
     color: #070707;
+`;
+
+const PrintButton = styled.div`
+    border-radius: 2em;
+    border: 2px solid #000;
+    background: #fff;
+    font-size: 1.125em;
+    font-weight: 700;
+    padding: 0.25em;
+    box-sizing: border-box;
+    cursor: pointer;
 `;
