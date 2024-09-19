3ce83eded7b2c7d3b0d136fdd06f2b6226c65fff
jiminseong <iamjms4237@gachon.ac.kr>
2024-09-19 13:42:52 +0900

    프린트 기능 추가예정

--- src/pages/test-result/TestResultPage.tsx
+++ src/pages/test-result/TestResultPage.tsx
@@ -1,5 +1,14 @@
+import styled from 'styled-components';
+
 const TestResultPage = () => {
-    return <div></div>;
+    return <PrintContainer>프린트할 공간이에요</PrintContainer>;
 };
 
 export default TestResultPage;
+
+const PrintContainer = styled.div`
+    width: 50%;
+    height: 50%;
+    background: #ffffff;
+    color: #070707;
+`;
